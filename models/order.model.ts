63c44368a919059a48abb8e9f5591b53985f0e04
Aniket <aniketkc@gmail.com>
2021-09-16 17:29:01 -0500

    Removed

--- models/order.model.ts
+++
@@ -1,3 +0,0 @@
-export class Order {
-    constructor(public _id: number, public total: number, public userId: number, public products: Array<string>, public status: string, public date: Date) { }
-}
